2c603efeda645411491bdae3ac58505bbc938eb7
Tara Sabella <tarasabella@gmail.com>
2018-12-18 22:55:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song # => define class named song 
+  # => each song instance has a name, artist & genre 
+  
+  attr_accessor :name, :artist, :genre 
+  #create a getter & setter so that they can be accessed w/in our methods and manipulated 
+  
+  @@count = 0
+  #class variable that keeps track of the number of new songs created 
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @@artist = [] 
+    @@genres = []
+    
+    #each individual song is initialized with a name, artist, and genre
+    #each instance of our class will have a name, artist & genre 
+    
+    @@count += 1 
+    @@artist << self 
+    @@genres.uniq << self 
+    #every new instance of the song class is added to the count integer 
+  
+   #each new instance of the song class is added to the genre array 
+  end 
+  
+  def genre_count 
+    @@genre.count 
+  end 
+    
+  
+  
+end 
